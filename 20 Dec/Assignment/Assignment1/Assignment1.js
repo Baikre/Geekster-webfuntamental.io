766b3395bc28cb8cb36c680c11e17bf03f194d4b
Baikre <hellomanik8670@gmail.com>
2021-12-31 23:31:45 +0530

    commit now

--- 20 Dec/Assignment/Assignment1/Assignment1.js
+++ 20 Dec/Assignment/Assignment1/Assignment1.js
@@ -1,14 +1,38 @@
+ 
+ 
+ 
+ /*Write a program to check whether the number is greater than 10 or not
 
 
 
 
+ Example 
+        input : 20 
+           output : 20 is greater than 10 
 
+        input : 7
+          output : 7 is less than 10
+
+        input : 10
+          output: 10 is equal 10 
+
+
+
+How to do ==> 
+              1. take a number from user 
+                 1.1 using prompt
+                 1.2 prompt  gives a string using parseInt to convert number datatype
+              2.  check a condition useing (if, else)
+                2.1 when number ==10 then (10 is equal 10)
+                2.2 when number > 10 then (number is greater than 10)
+                2.3when nuber < 10 then (number is less than 10)
+*/
 let num = prompt("Enter a number")
 num = parseInt(num);
 if(num == 10){
     console.log(num + " is  Equal  10");
 }
-else if (num >= 10) {
+else if (num > 10) {
     console.log(num + " is greater then 10");
 }
 else {
--- 20 Dec/Assignment/Assignment2/Assignment2.js
+++ 20 Dec/Assignment/Assignment2/Assignment2.js
@@ -1,4 +1,34 @@
+/*
+Q2. Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
 
+
+
+
+Example  : 
+input =  20 , 10 ,30
+ output =  30 is greatest than other
+
+ input =  82,76,58
+ output = 82 is greatest than othe  
+
+ How to do 1st approach==>
+         1 take 3 input from user using prompt
+            
+        2 promt gives string using parseInt cnovert number datatype
+            
+        3 using condition 
+            if( num1 > num2 and num1 > num3 )= true num1 greatest
+            if( num2 > num1 and num2 > num3 )= true num2 greatest
+            if( num3 > num1 and num3 > num1 )= true num3 greatest  
+            
+
+        2nd approach==>
+        1 take 3 input from user using prompt
+        2 promt gives string using parseInt cnovert number datatype
+             
+        3 And  in console.log( Math.max(num1,num2,num3))
+                
+*/
 let num1 = prompt("Enter a number 1st");
 num1 = parseInt(num1);
 let num2 = prompt("Enter a number 2nd");
