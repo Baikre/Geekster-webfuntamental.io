7ca9b57279f0accca7eb089cb419bad73e240712
Baikre <hellomanik8670@gmail.com>
2022-01-01 13:48:30 +0530

    second commit

--- 21 Dec/Assignment/Assignment1/Assignment1.js
+++ 21 Dec/Assignment/Assignment1/Assignment1.js
@@ -1,4 +1,33 @@
+/* 
 
+Q. Take 3 inputs from the user and show minimum of the three.
+
+Example  : 
+input =  20 , 10 ,30
+ output =  10 is minimum  than other
+
+ input =  82,76,58
+ output = 58 is minimum  than othe  
+
+ How to do 1st approach==>
+         1 take 3 input from user using prompt
+            
+        2 promt gives string using parseInt cnovert number datatype
+            
+        3 using condition 
+            if( num1 < num2 and num1 < num3 )= true num1 is minimum  than other
+            if( num2 < num1 and num2 < num3 )= true num2 is minimum  than other
+            if( num3 < num1 and num3 < num1 )= true num3 is minimum  than other
+            
+
+        2nd approach==>
+        1 take 3 input from user using prompt
+        2 promt gives string using parseInt cnovert number datatype
+             
+        3 And  in console.log( Math.min(num1,num2,num3))
+                
+*/
+let num1 = prompt("Enter a number 1st");
 
 
 let num1 = prompt("Enter a number 1st");
@@ -9,15 +38,15 @@ let num3 = prompt("Enter a number 3rd");
 num3 = parseInt(num3);
 
 if (num1 < num2 && num1 < num3) {
-    console.log("Number 1st = " + num1 + " is minimum of three.");
+    console.log("Number 1st = " + num1 + " is minimum  than other.");
 
 }
 else if (num2 < num1 && num2 < num3) {
-    console.log("Number 2nd = " + num2 + " is minimum of three.");
+    console.log("Number 2nd = " + num2 + " is minimum  than other.");
 
 }
 else if (num3 < num1 && num3 < num2) {
-    console.log("Number 3rd = " + num3 + " is minimum of three.");
+    console.log("Number 3rd = " + num3 + " minimum  than other.");
 }
 else {
     console.log(" Number 1st 2nd 3rd is Equal Number");
--- 21 Dec/Assignment/Assignment2/Assignment2.js
+++ 21 Dec/Assignment/Assignment2/Assignment2.js
@@ -1,24 +1,56 @@
+/*
 
 
+Check whether the given chracter is a vowel or not. (Also check for capital characters)
 
 
+Example : 
+          input : U 
+          output : character is vowel 
+          input : c
+          output : character is not vowel
+          input : e
+          output : character is vowel
+          
+  how to do ==>
+          
+            1 taking input from user using prompt
+            2. cheking 
+              2.1.character is (a or A) = true then  output is vowel
+              2.2 character is (e or E) = true then output is vowel
+              2.3 character is (i or I) = true then output is vowel
+              2.4 character is (o or O) = true then output is vowel
+              2.3 character is (u or U) = true then output is vowel
+              2.4  character is not (a,e,i,o,u) or (A,E,I,O,U) then output character is not vowel
+
+              
+
+
+*/
+
+  // input from user 
 let char = prompt("Enter a chracter")
-
+  // cheking char is "a" or "A"
 if ((char == "a") || (char == "A")) {
-    alert("chracter is  vowel ");
+    alert(char + " chracter is  vowel ");
 }
+  // cheking char is "e"  or "E"
 else if ((char == "e") || (char == "E")) {
-    alert("chrcter is vowel");
+    alert(char +" chrcter is vowel");
 }
+  // cheking char is "i" or "I"
 else if ((char == "i") || (char == "I")) {
-    alert("chrcter is vowel");
+    alert(char +" chrcter is vowel");
 }
+  // cheking char is "o" or "O"
 else if ((char == "o") || (char == "O")) {
-    alert("chrcter is vowel");
+    alert(char +" chrcter is vowel");
 }
+  // cheking char is "u" or "U"
 else if ((char == "u") || (char == "U")) {
-    alert("chrcter is vowel");
+    alert(char +" chrcter is vowel");
 }
+  //  char not a,e,i,o,u then  chrcter is not vowel
 else {
-    alert("chrcter is consonant");
+    alert(char +" chrcter is not vowel");
 }
