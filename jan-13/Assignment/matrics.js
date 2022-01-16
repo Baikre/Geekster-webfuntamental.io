6bae67ced9472f46614949b235bfae0007099c26
Baikre <hellomanik8670@gmail.com>
2022-01-16 16:42:59 +0530

    jan-13

--- jan-13/Assignment/matrics.js
+++ jan-13/Assignment/matrics.js
@@ -1,20 +1,21 @@
 let array = [[4, 3, 2, 4],
-        [9, 1, 5, 6],
-        [2, 7, 9, 6],
-        [8, 5, 2, 0]];
+            [9, 1, 5, 6],
+            [2, 7, 9, 6],
+            [8, 5, 2, 0]];
+
+        var addition = 0;
 
         for (let i = 0; i < array.length; i++) {
 
-            let colum = array[i]
-            // console.log(colum);
+          
             for (let j = 0; j < colum.length; j++) {
 
-                let m = colum[j]
-
+             
                 if (i == j) {
-                   array[i][j]
+                   addition = addition + array[i][j]
                 }
               
             }
 
         }
+console.log(addition);
