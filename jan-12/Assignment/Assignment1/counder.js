c2b40702aa306a7ed1104c2fe7928929af902d08
Baikre <hellomanik8670@gmail.com>
2022-01-16 08:17:38 +0530

    jan-12

---
+++ jan-12/Assignment/Assignment1/counder.js
@@ -0,0 +1,17 @@
+// document.write("🎈")
+
+let input1 = document.getElementById("like_counder")
+function like(){
+//     input1.value = input.value + 1
+     
+    input1.value = parseInt(input1.value)+ 1
+
+
+}
+
+let input2 = document.getElementById("dislike_counder");
+
+function dislike(){
+   input2.value = parseInt(input2.value) + 1
+     
+}
